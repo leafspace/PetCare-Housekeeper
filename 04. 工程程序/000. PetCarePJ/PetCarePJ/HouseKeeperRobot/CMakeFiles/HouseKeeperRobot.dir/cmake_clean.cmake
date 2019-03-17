b7113bf934c182f23c242d1695268b8dfee45487
leafspace <leafspace_zhang@163.com>
2019-03-17 18:08:35 +0800

    代码更新-添加了工程的cmakefile文件

---
+++ 04. 工程程序/000. PetCarePJ/PetCarePJ/HouseKeeperRobot/CMakeFiles/HouseKeeperRobot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HouseKeeperRobot.dir/SourceMain.cpp.o"
+  "HouseKeeperRobot.pdb"
+  "HouseKeeperRobot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HouseKeeperRobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
